e01f601cdd410d4c22af5178af72a236c7ab9d76
duanjunfeng <488698112@qq.com>
2020-12-10 10:08:36 +0800

    init

---
+++ src/input/input.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a-input plachholder="请输入内容"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="root">
+    <div v-for="(item,index) of list" :key='index'>{{item}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      list: ['a', 'b', 'c']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
